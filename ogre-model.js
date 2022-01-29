4d70534501f5c11d16dfa5b0e8a5c6f8f5297029
Matteo Radice <matteo.radice@docebo.com>
2022-01-29 21:14:19 +0100

    added json description of ogres

---
+++ ogre-model.js
@@ -0,0 +1,117 @@
+const ogreModels = [{
+    model: "OGRE MK. III",
+    name: "OGRE MK. III",
+    faction: "combine",
+    au: "17",
+    size: "7",
+    weapons: [
+        {
+            name: "Main Battery",
+            slots: 1,
+            remaining: 1,
+            atk: 4,
+            rng: 3,
+            def: 4
+        },
+        {
+            name: "Secondary Battery",
+            slots: 4,
+            remaining: 4,
+            atk: 3,
+            rng: 2,
+            def: 3
+        },
+        {
+            name: "Missiles",
+            slots: 2,
+            remaining: 2,
+            atk: 6,
+            rng: 5,
+            def: 3
+        },
+        {
+            name: "Antipersonnel",
+            slots: 8,
+            remaining: 8,
+            atk: 1,
+            rng: 1,
+            def: 1
+        },
+    ],
+    movement: [
+        {
+            range: 3,
+            max: 45,
+            remaining: 15,
+        },
+        {
+            range: 2,
+            max: 30,
+            remaining: 15,
+        },
+        {
+            range: 1,
+            max: 15,
+            remaining: 15,
+        }
+
+    ]
+},
+{
+    model: "OGRE MK. V",
+    name: "OGRE MK. V",
+    faction: "combine",
+    au: "25",
+    size: "8",
+    weapons: [
+        {
+            name: "Main Battery",
+            slots: 2,
+            remaining: 2,
+            atk: 4,
+            rng: 3,
+            def: 4
+        },
+        {
+            name: "Secondary Battery",
+            slots: 4,
+            remaining: 4,
+            atk: 3,
+            rng: 2,
+            def: 3
+        },
+        {
+            name: "Missiles",
+            slots: 6,
+            remaining: 6,
+            atk: 6,
+            rng: 5,
+            def: 3
+        },
+        {
+            name: "Antipersonnel",
+            slots: 12,
+            remaining: 12,
+            atk: 1,
+            rng: 1,
+            def: 1
+        },
+    ],
+    movement: [
+        {
+            range: 3,
+            max: 60,
+            remaining: 20,
+        },
+        {
+            range: 2,
+            max: 40,
+            remaining: 20,
+        },
+        {
+            range: 1,
+            max: 20,
+            remaining: 20,
+        }
+    ]
+}];
